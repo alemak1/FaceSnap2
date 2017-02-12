d6a344fe1d33da5defcad32c8edc7f277dc6e4e3
yongrenwang <czie.alexmak@gmail.com>
2017-02-12 20:25:07 +0800

    Instantiated a UIWindow programmatically in the AppDelegate file, in the didFinishLaunchingWithOptions delegate method; also assigned a navigation controller as the root view controller for the window, and for the initialization of the root view controller, a customized view controller (i.e. a custom type subclassing UIViewController was used for the initialization of the navigation controller

---
+++ FaceSnapp2/FaceSnapp2/PhotoListController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceSnapp2
+//
+//  Created by Aleksander Makedonski on 2/12/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
