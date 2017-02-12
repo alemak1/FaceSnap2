ad69e08e1c67bd018681a6e84edb1d24fc749c96
yongrenwang <czie.alexmak@gmail.com>
2017-02-13 01:07:29 +0800

    Implemented PhotoFilterController class

---
+++ FaceSnapp2/PhotoFilterController.swift
@@ -0,0 +1,95 @@
+//
+//  PhotoFilterController.swift
+//  FaceSnapp2
+//
+//  Created by Aleksander Makedonski on 2/13/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFilterController: UIViewController {
+
+    private var mainImage: UIImage
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private lazy var filterHeaderLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Select a filter"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    lazy var filtersCollectionView: UICollectionView = {
+       let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumLineSpacing = 10
+        flowLayout.minimumInteritemSpacing = 1000
+        flowLayout.itemSize = CGSize(width: 100, height: 100)
+        
+        let collectionView = UICollectionView(frame: CGRect(origin: CGPoint(x: 0, y:0), size: CGSize(width: 0, height: 0)), collectionViewLayout: flowLayout)
+        collectionView.backgroundColor = UIColor.white
+        return collectionView
+    }()
+    
+    init(image: UIImage){
+        self.mainImage = image
+        self.photoImageView.image = mainImage
+        super.init(nibName: nil, bundle: nil)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    override func viewWillLayoutSubviews() {
+        photoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(photoImageView)
+        
+        filterHeaderLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filterHeaderLabel)
+        
+        filtersCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(filtersCollectionView)
+        
+        NSLayoutConstraint.activate([
+            filtersCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            filtersCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            filtersCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            filtersCollectionView.heightAnchor.constraint(equalToConstant: 200.0),
+            filtersCollectionView.topAnchor.constraint(equalTo: filterHeaderLabel.bottomAnchor),
+            filterHeaderLabel.leftAnchor.constraint(equalTo: view.leftAnchor),
+            filterHeaderLabel.rightAnchor.constraint(equalTo: view.rightAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: filtersCollectionView.topAnchor),
+            photoImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            photoImageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            photoImageView.rightAnchor.constraint(equalTo: view.rightAnchor)
+            ])
+    }
+
+}
